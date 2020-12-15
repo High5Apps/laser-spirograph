eeecc42cc0fafb62d1d440d7dd399845bd4380a4
High5Apps <high5apps@gmail.com>
2020-12-14 17:42:47 -0800

    Draw a dot when function is constant

--- laser-spirograph/views/LSCanvas.swift
+++ laser-spirograph/views/LSCanvas.swift
@@ -9,49 +9,79 @@ import UIKit
 
 class LSCanvas: UIView {
     
+    // MARK: Properties
+    
     var color: UIColor = .green
     var parametricFunction: Parameterizable = LSConstantFunction(x: 1, y: 0)
     
-    private let shapeLayer = CAShapeLayer()
+    private var shapeLayer = CAShapeLayer()
     private var pathTransform: CGAffineTransform = .identity
+    private var canvasRadius: CGFloat = 1
         
     private static let persistenceOfVision: TimeInterval = 1 / 16
     private static let stepSize: Double = persistenceOfVision / pow(2, 8)
     private static let lineWidth: CGFloat = 4
     private static let semiLineWidth: CGFloat = lineWidth / 2
     
+    // MARK: Initialization
+    
     required init?(coder: NSCoder) {
         super.init(coder: coder)
-        
-        shapeLayer.fillColor = nil
-        shapeLayer.lineWidth = Self.lineWidth
-        shapeLayer.strokeColor = color.cgColor
         layer.addSublayer(shapeLayer)
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        shapeLayer.frame = bounds
-        pathTransform = getTransformFromUnitCircleToRect(bounds)
+        canvasRadius = getCanvasRadius()
+        pathTransform = getTransformFromUnitCircleToRect()
     }
     
-    private func getTransformFromUnitCircleToRect(_ rect: CGRect) -> CGAffineTransform {
-        let insetRect = rect.insetBy(dx: Self.semiLineWidth, dy: Self.semiLineWidth)
-        
+    private func getCanvasRadius() -> CGFloat {
+        let insetRect = bounds.insetBy(dx: Self.semiLineWidth, dy: Self.semiLineWidth)
+        return min(insetRect.size.height, insetRect.size.width) / 2
+    }
+    
+    private func getTransformFromUnitCircleToRect() -> CGAffineTransform {
         let shiftAxesTransform = CGAffineTransform(translationX: 1, y: 1)
-
-        let radius: CGFloat = min(insetRect.size.height, insetRect.size.width) / 2
-        let scaleTransform = CGAffineTransform(scaleX: radius, y: radius)
-        
+        let scaleTransform = CGAffineTransform(scaleX: canvasRadius, y: canvasRadius)
         let strokeOffsetTransform = CGAffineTransform(translationX: Self.semiLineWidth, y: Self.semiLineWidth)
         return shiftAxesTransform.concatenating(scaleTransform).concatenating(strokeOffsetTransform)
     }
+    
+    // MARK: Drawing
         
     func draw(time: Double) {
         updateShapeLayer(time)
     }
     
     private func updateShapeLayer(_ time: Double) {
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.frame = bounds
+        shapeLayer.lineWidth = Self.lineWidth
+        
+        let isConstant = parametricFunction.isConstant()
+        
+        let bezierPath = isConstant ? getConstantPath(time) : getVariablePath(time)
+        bezierPath.apply(pathTransform)
+        shapeLayer.path = bezierPath.cgPath
+        
+        shapeLayer.strokeColor = isConstant ? nil : UIColor.green.cgColor
+        shapeLayer.fillColor = isConstant ? UIColor.green.cgColor : nil
+        
+        layer.replaceSublayer(self.shapeLayer, with: shapeLayer)
+        self.shapeLayer = shapeLayer
+    }
+    
+    private func getConstantPath(_ time: Double) -> UIBezierPath {
+        let path = UIBezierPath()
+        
+        let pathRadius = Self.semiLineWidth / canvasRadius
+        path.addArc(withCenter: getCGPoint(parametricFunction, time), radius: pathRadius, startAngle: 0, endAngle: 2 * .pi, clockwise: true)
+        
+        return path
+    }
+    
+    private func getVariablePath(_ time: Double) -> UIBezierPath {
         let path = UIBezierPath()
         
         let t0 = time - Self.persistenceOfVision
@@ -61,9 +91,7 @@ class LSCanvas: UIView {
             path.addLine(to: getCGPoint(parametricFunction, t))
         }
         
-        path.apply(pathTransform)
-
-        shapeLayer.path = path.cgPath
+        return path
     }
     
     private func getCGPoint(_ parametricFunction: Parameterizable, _ time: Double) -> CGPoint {
