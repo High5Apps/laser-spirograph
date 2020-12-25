ccf3e78c3414df0bbe24a1ec99da4d272b2b1acf
High5Apps <high5apps@gmail.com>
2020-12-24 17:39:35 -0800

    Update color to match green laser wavelength

--- laser-spirograph/SceneDelegate.swift
+++ laser-spirograph/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
         window?.overrideUserInterfaceStyle = .dark
-        window?.tintColor = .green
+        window?.tintColor = UIColor(red: 101/255.0, green: 1.0, blue: 0.0, alpha: 1.0)
         window?.makeKeyAndVisible()
         
         guard let viewController = window?.rootViewController as? ViewController, let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
