9b6338f47dcb07ea6e3c2b7c2dfe43b45795af52
High5Apps <high5apps@gmail.com>
2021-01-31 20:00:04 -0800

    Add iconView to LSColorChipCVC

--- laser-spirograph/views/LSColorChipCollectionViewCell.swift
+++ laser-spirograph/views/LSColorChipCollectionViewCell.swift
@@ -7,14 +7,28 @@
 
 import UIKit
 
+enum ColorChipIcon: String {
+    case plus
+}
+
 class LSColorChipCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: Properties
     
     var color: UIColor? {
         didSet { contentView.backgroundColor = color }
     }
     
+    var icon: ColorChipIcon? {
+        didSet { setIcon(named: icon?.rawValue) }
+    }
+    
+    private var iconView: UIImageView?
+    
     private static let margin: CGFloat = 16
     private static let halfMargin = margin / 2
+    
+    // MARK: Initialization
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
@@ -24,6 +38,16 @@ class LSColorChipCollectionViewCell: UICollectionViewCell {
         contentView.layer.cornerRadius = Self.halfMargin
         contentView.layer.borderColor = UIColor.label.cgColor
         
+        setSelectedBackgroundView()
+        addIconView()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        selectedBackgroundView?.frame = contentView.bounds.insetBy(dx: -Self.halfMargin, dy: -Self.halfMargin)
+    }
+    
+    private func setSelectedBackgroundView() {
         let backgroundView = UIView()
         backgroundView.backgroundColor = .label
         backgroundView.layer.cornerRadius = Self.margin
@@ -32,8 +56,28 @@ class LSColorChipCollectionViewCell: UICollectionViewCell {
         selectedBackgroundView = backgroundView
     }
     
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        selectedBackgroundView?.frame = contentView.bounds.insetBy(dx: -Self.halfMargin, dy: -Self.halfMargin)
+    private func addIconView() {
+        let iconView = UIImageView()
+        iconView.tintColor = .label
+        iconView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(iconView)
+        
+        iconView.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.5).isActive = true
+        iconView.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.5).isActive = true
+        iconView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
+        iconView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        
+        self.iconView = iconView
+    }
+    
+    // MARK: Icons
+    
+    private func setIcon(named systemName: String?) {
+        guard let systemName = systemName else {
+            iconView?.image = nil
+            return
+        }
+        
+        iconView?.image = UIImage(systemName: systemName)
     }
 }
